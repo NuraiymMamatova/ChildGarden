b183ee09e3f6bcbcd4220035961900d1e92e2a1c
Mamatova Nuraiym <mnurajym9@gmail.com>
2023-09-17 12:19:11 +0600

    in properties added server.port=1234 and in data.sql file added some strings that insert user data and connect it with role

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -4,3 +4,9 @@ insert into role(id, role_name)
 VALUES (2, 'CHILD_GARDEN');
 insert into role(id, role_name)
 VALUES (3, 'TEACHER');
+
+insert into users(id, email, image, name, password)
+VALUES (1, 'john@doe.com', 'https://static.vecteezy.com/system/resources/previews/007/409/979/original/people-icon-design-avatar-icon-person-icons-people-icons-are-set-in-trendy-flat-style-user-icon-set-vector.jpg', 'John', '$2a$12$q1kp2Y56qZKXi/OjCuPNXeDvy.ko0ue90cgsilzsOwM7yhEwoJWNq');
+
+insert into roles_users(role_id, user_id)
+VALUES (2, 1);
